d05b2c52a40ced1ade083f7bb5fc95ef7df367b6
Paulimar Alvarado <paalvarado@fibex.local>
2026-09-04 14:04:51 -0400

    Restructure: frontend at root for Vercel, backend moved to separate repo

--- backend/models/Folder.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const folderSchema = new mongoose.Schema({
-  nombre: { type: String, required: true },
-  descripcion: { type: String, default: '' },
-  usuarios: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
-}, { timestamps: true });
-
-module.exports = mongoose.model('Folder', folderSchema);
